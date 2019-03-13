30b7c786c065a470ac68ac0a844092b99d898b51
user <adellasalsa91@gmail.com>
2019-03-13 13:30:00 +0700

    file pertama

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <h2>Hello Nama Saya Adella!</h2>
+        <Gambar />
+      </div>
+    )
+  }
+}
+
+export default App
